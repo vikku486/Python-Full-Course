acf7e83bebd880e7919744deb3a0943f955e1f04
Vikas Sagar <89192218+vikku486@users.noreply.github.com>
2025-05-25 20:59:04 +0530

    Update 1_Introduction.py

--- 1_Introduction.py
+++ 1_Introduction.py
@@ -1,18 +1,15 @@
 """
-🙅‍♂️ Python is a general-purpose computer programming language based on object-oriented programming. The
-programs in Python can run equally on every operating system. It is the primary language used in modern
-technologies like AI, data science, and ML. It was developed in 1991 by Guido van Rossum.
+🙅‍♂️ Python is a general-purpose computer programming language based on object-oriented programming. The programs in Python can run equally on every 
+operating system. It is the primary language used in modern technologies like AI, data science, and ML. It was developed in 1991 by Guido van Rossum.
 
 ⭐ Is Python code compiled or interpreted?
-Python code is interpreted. A Python code is converted into bytecode that is executed by a virtual machine in
-Python. A Python program is not needed to build like code for other languages where it is compiled, and it
-works on an interpreter only.
+Python code is interpreted. A Python code is converted into bytecode that is executed by a virtual machine in Python. A Python program is not needed 
+to build like code for other languages where it is compiled, and it works on an interpreter only.
 
 ⭐ What is an Interpreted language?
-When the implementation of the programs is done directly without compilation of the program into machine
-language, the language is called interpreted language. These languages are slower than the compiled languages.
-All the debugging occurs at run time in this language. Python, JavaScript, Perl, etc., are some of the examples
-of interpreted languages.
+When the implementation of the programs is done directly without compilation of the program into machine language, the language is called interpreted 
+language. These languages are slower than the compiled languages. All the debugging occurs at run time in this language. Python, JavaScript, Perl, etc., 
+are some of the examples of interpreted languages.
 
 👉 Python is a case-sensitive language.
 
@@ -32,18 +29,6 @@ print("hello", "Vikas", "Sagar", sep="/")
 
 print("Hello", end="-")
 print("Vikas")
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
